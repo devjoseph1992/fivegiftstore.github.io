6265e062df7e539b092c6d7849bf4abd2a2c001f
John Joseph Florina <devjosephflorina@gmail.com>
2021-02-05 11:20:26 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      john
+    </div>
+  );
+}
+
+export default App;
